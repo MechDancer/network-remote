6b1128cb08641c21a043c820f48ee397312ee758
berberman <1793913507@qq.com>
2019-05-08 19:11:00 +0800

    add group

--- src/Network/Remote.hs
+++ src/Network/Remote.hs
@@ -1,1 +1,10 @@
 module Network.Remote where
+
+import Data.Int (Int64)
+import Data.Time.Clock.System
+import Data.Word (Word32)
+
+type Name = String
+
+currentTimeSeconds :: IO Int64
+currentTimeSeconds = return 2333 -- TODO
---
+++ src/Network/Remote/Group.hs
@@ -0,0 +1,30 @@
+module Network.Remote.Group where
+
+import Data.IORef
+import Data.Int (Int64)
+import qualified Data.Map as M
+import Network.Remote (Name, currentTimeSeconds)
+
+newtype Group = Gp
+  { _core :: IORef (M.Map String Int64)
+  }
+
+newGroup :: IO Group
+newGroup = newIORef M.empty >>= \core -> return $ Gp core
+
+detect :: Group -> Name -> IO (Name, Int64)
+detect group name = do
+  t <- currentTimeSeconds
+  modifyIORef (_core group) $ \m -> M.insert name t m
+  return (name, t)
+
+get :: Group -> Name -> IO (Maybe Int64)
+get group name = do
+  m <- readIORef $ _core group
+  return $ M.lookup name m
+
+getByTimeout :: Group -> Int64 -> IO [Name]
+getByTimeout group timeout = do
+  now <- currentTimeSeconds
+  m <- readIORef $ _core group
+  return . M.keys $ M.filterWithKey (\name time -> now - time < timeout) m
