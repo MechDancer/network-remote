70947b39d3d810dd5819fb00aa0387bb9fbeed75
berberman <1793913507@qq.com>
2019-05-11 14:08:14 +0800

    fix multicast socket to stream

--- src/Network/Remote/Resource/SocketStream.hs
+++ src/Network/Remote/Resource/SocketStream.hs
@@ -1,18 +1,41 @@
-module Network.Remote.Resource.SocketStream where
+module Network.Remote.Resource.SocketStream
+  ( SocketStream(..)
+  , multicastSocketToStream
+  ) where
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Internal as S
 import Network.Remote.Resource.MulticastSocket
+import Network.Socket (SockAddr)
 import qualified Network.Socket.ByteString as B
-import System.IO.Streams
-import System.IO.Streams.Network
+import qualified System.IO.Streams as Streams
+import System.IO.Streams (InputStream, OutputStream)
 
 data SocketStream = SocketStream
-  { inputStream :: InputStream ByteString
+  { inputStream :: InputStream (ByteString, SockAddr)
   , outputStream :: OutputStream ByteString
   }
 
 multicastSocketToStream :: MulticastSocket -> IO SocketStream
-multicastSocketToStream MulticastSocket {sender = sender, receiver = receiver} = do
-  (input, _) <- socketToStreams receiver
-  (_, output) <- socketToStreams sender
+multicastSocketToStream (MulticastSocket sender receiver addr) = do
+  input <- socketToStreamsInternalI receiver
+  output <- socketToStreamsInternalO sender addr
   return $ SocketStream input output
+
+socketToStreamsInternalI socket = Streams.makeInputStream input
+  where
+    input = do
+      (s, addr) <- B.recvFrom socket 4096
+      return $!
+        if S.null s
+          then Nothing
+          else Just (s, addr)
+
+socketToStreamsInternalO socket addr = Streams.makeOutputStream output
+  where
+    output Nothing = return ()
+    output (Just s) =
+      if S.null s
+        then return ()
+        else B.sendAllTo socket s addr
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,18 @@
-import Control.Monad (join)
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (forever, join)
 import qualified Data.ByteString.Char8 as B
+import Network.Multicast
 import Network.Remote.Resource.MulticastSocket
 import Network.Remote.Resource.Networks (scanNetwork)
 import Network.Remote.Resource.SocketStream
+import Network.Socket
 import qualified System.IO.Streams as S
 
 main :: IO ()
 main = do
   manager <- newManager "233.233.233.233" 23333
   stream <- join $ head <$> scanNetwork >>= (fmap multicastSocketToStream . getWithInterface manager)
-  S.write (pure $ B.pack "锟斤拷") $ outputStream stream
+  S.write (pure $ B.pack "sdsdsd") $ outputStream stream
   S.read (inputStream stream) >>= print
+  print "GG"
