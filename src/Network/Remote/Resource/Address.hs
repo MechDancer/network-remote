d512b92f504b26b8757d2df9ae6c3cec2bc0c362
berberman <1793913507@qq.com>
2019-05-16 21:03:15 +0800

    use ReaderT in Address and Group

--- src/Network/Remote/Resource/Address.hs
+++ src/Network/Remote/Resource/Address.hs
@@ -1,6 +1,8 @@
 module Network.Remote.Resource.Address where
 
 import Control.Concurrent.MVar (MVar, modifyMVar_, newMVar, readMVar)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Reader (ReaderT(..))
 import qualified Data.Map as M
 import Network.Remote.Protocol (Name)
 import Network.Socket (HostAddress, PortNumber, SockAddr, SockAddr(SockAddrInet), tupleToHostAddress)
@@ -12,29 +14,33 @@ newtype Addresses = Ad
 newAddresses :: IO Addresses
 newAddresses = Ad <$> newMVar M.empty
 
-insertSockAddr :: Addresses -> Name -> SockAddr -> IO ()
-insertSockAddr (Ad var) name addr = modifyMVar_ var $ \m -> return $ M.insert name addr m
+insertSockAddr :: (MonadIO m) => Name -> SockAddr -> ReaderT Addresses m ()
+insertSockAddr name addr = ReaderT $ \(Ad var) -> liftIO $ modifyMVar_ var $ \m -> return $ M.insert name addr m
 
-getSockAddr :: Addresses -> Name -> IO (Maybe SockAddr)
-getSockAddr (Ad var) name = readMVar var >>= \m -> return $ m M.!? name
+getSockAddr :: (MonadIO m) => Name -> ReaderT Addresses m (Maybe SockAddr)
+getSockAddr name = ReaderT $ \(Ad var) -> liftIO $readMVar var >>= \m -> return $ m M.!? name
 
-insertPort :: Addresses -> Name -> PortNumber -> IO ()
-insertPort (Ad var) name port =
-  modifyMVar_ var $ \m ->
-    return $
-    compute m name $ \_ vv ->
-      case vv of
-        Nothing -> SockAddrInet port $ tupleToHostAddress (0, 0, 0, 0)
-        Just (SockAddrInet _ addr) -> SockAddrInet port addr
+insertPort :: (MonadIO m) => Name -> PortNumber -> ReaderT Addresses m ()
+insertPort name port =
+  ReaderT $ \(Ad var) ->
+    liftIO $
+    modifyMVar_ var $ \m ->
+      return $
+      compute m name $ \_ vv ->
+        case vv of
+          Nothing -> SockAddrInet port $ tupleToHostAddress (0, 0, 0, 0)
+          Just (SockAddrInet _ addr) -> SockAddrInet port addr
 
-insertAddr :: Addresses -> Name -> HostAddress -> IO ()
-insertAddr (Ad var) name addr =
-  modifyMVar_ var $ \m ->
-    return $
-    compute m name $ \_ vv ->
-      case vv of
-        Nothing -> SockAddrInet 0 addr
-        Just (SockAddrInet port _) -> SockAddrInet port addr
+insertAddr :: (MonadIO m) => Name -> HostAddress -> ReaderT Addresses m ()
+insertAddr name addr =
+  ReaderT $ \(Ad var) ->
+    liftIO $
+    modifyMVar_ var $ \m ->
+      return $
+      compute m name $ \_ vv ->
+        case vv of
+          Nothing -> SockAddrInet 0 addr
+          Just (SockAddrInet port _) -> SockAddrInet port addr
 
 compute :: Ord k => M.Map k v -> k -> (k -> Maybe v -> v) -> M.Map k v
 compute m k f = M.insert k (f k $ m M.!? k) m
--- src/Network/Remote/Resource/Group.hs
+++ src/Network/Remote/Resource/Group.hs
@@ -6,33 +6,40 @@ module Network.Remote.Resource.Group
   , getByTimeout
   ) where
 
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Trans.Reader (ReaderT(..))
 import Data.IORef
 import Data.Int (Int64)
 import qualified Data.Map as M
 import Network.Remote (currentTimeSeconds)
 import Network.Remote.Protocol (Name)
 
-newtype Group =
-  Gp
-    { _core :: IORef (M.Map String Int64)
-    }
+newtype Group = Gp
+  { _core :: IORef (M.Map String Int64)
+  }
 
 newGroup :: IO Group
 newGroup = Gp <$> newIORef M.empty
 
-detect :: Group -> Name -> IO (Name, Int64)
-detect group name = do
-  t <- currentTimeSeconds
-  modifyIORef (_core group) $ \m -> M.insert name t m
-  return (name, t)
+detect :: (MonadIO m) => Name -> ReaderT Group m (Name, Int64)
+detect name =
+  ReaderT $ \group ->
+    liftIO $ do
+      t <- currentTimeSeconds
+      modifyIORef (_core group) $ \m -> M.insert name t m
+      return (name, t)
 
-get :: Group -> Name -> IO (Maybe Int64)
-get group name = do
-  m <- readIORef $ _core group
-  return $ M.lookup name m
+get :: (MonadIO m) => Name -> ReaderT Group m (Maybe Int64)
+get name =
+  ReaderT $ \group ->
+    liftIO $ do
+      m <- readIORef $ _core group
+      return $ M.lookup name m
 
-getByTimeout :: Group -> Int64 -> IO [Name]
-getByTimeout group timeout = do
-  now <- currentTimeSeconds
-  m <- readIORef $ _core group
-  return . M.keys $ M.filterWithKey (\name time -> now - time < timeout) m
+getByTimeout :: (MonadIO m) => Int64 -> ReaderT Group m [Name]
+getByTimeout timeout =
+  ReaderT $ \group ->
+    liftIO $ do
+      now <- currentTimeSeconds
+      m <- readIORef $ _core group
+      return . M.keys $ M.filterWithKey (\name time -> now - time < timeout) m
