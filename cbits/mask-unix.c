6506fe95b35601cb2338a839c5478d1142f987ab
berberman <1793913507@qq.com>
2019-12-01 15:26:02 +0800

    fix mask on unix

--- cbits/mask-unix.c
+++ cbits/mask-unix.c
@@ -1,23 +1,24 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <ifaddrs.h>
-#include <stdio.h>
 #include <string.h>
 
 char *c_get_subnet_mask(const char *interface_ip_address) {
     struct ifaddrs *ifap, *ifa;
-    struct sockaddr_in *sa;
-    char *addr = NULL;
+    struct sockaddr_in *sa, *addr;
+    char *mask = NULL;
 
     getifaddrs(&ifap);
     for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
         if (ifa->ifa_addr->sa_family == AF_INET) {
             sa = (struct sockaddr_in *) ifa->ifa_netmask;
-            addr = inet_ntoa(sa->sin_addr);
-            if (strcmp(interface_ip_address, ifa->ifa_addr->ifa_name) == 0)
+            addr = (struct sockaddr_in *) ifa->ifa_addr;
+            if (strcmp(interface_ip_address, inet_ntoa(addr->sin_addr)) == 0) {
+                mask = inet_ntoa(sa->sin_addr);
                 break;
+            }
         }
     }
     freeifaddrs(ifap);
-    return addr;
+    return mask;
 }
